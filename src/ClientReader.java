d3c3f7a0de909b2104618e096942f451e125060f
Zied <ziedmaghraoui58@gmail.com>
2024-04-07 23:46:35 +0100

    fixed Read Last line request

--- src/ClientReader.java
+++ src/ClientReader.java
@@ -1,63 +1,45 @@
 import com.rabbitmq.client.*;
-import com.rabbitmq.client.BuiltinExchangeType;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.TimeoutException;
 
 public class ClientReader {
-    private static final String QUEUE_NAME = "reader_queue";
-    private final static String EXCHANGE_NAME = "read_exchange";
+    protected static final String QUEUE_NAME = "reader_queue";
+    protected static final String ROUTING_KEY = "reader";
+    protected final static String EXCHANGE_NAME = "read_exchange";
 
-
-    public static void main(String[] argv) throws Exception {
-
-        readLast();
-        processRead();
-
+    public static void main(String[] argv) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            requestReadLast(channel);
+            processRead(channel);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private static void processRead() throws IOException, TimeoutException {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost"); // Adresse du broker RabbitMQ
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-        // Declare a queue for receiving the responses
+    private static void processRead(Channel channel) throws IOException {
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-        channel.exchangeDeclare("direct_read", BuiltinExchangeType.DIRECT);
-        // Bind the queue to the exchange
-        channel.queueBind(QUEUE_NAME, "direct_read", "");
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+        channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
 
-        System.out.println(" [Client reader] Waiting for messages. To exit press CTRL+C");
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        // Set up a consumer to consume messages from the queue
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-            System.out.println(" [Client reader] Received '" + message + "'");
+            System.out.println(" [*] Received '" + message + "'");
         };
 
-        // Consume messages from the queue
         channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
         });
     }
 
-    private static void readLast() throws Exception {
-        // Établir une connexion à RabbitMQ
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost"); // Adresse du broker RabbitMQ
-        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
-            // Déclarer un échange (exchange) de type fanout
-            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
-
-            // Message à envoyer (ligne de texte à ajouter)
-            String message = "Read Last";
-
-            // Publier le message sur l'échange en mode fanout
-            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes(StandardCharsets.UTF_8));
-            System.out.println(" [Client reader] Sent '" + message + "'");
-        }
+    private static void requestReadLast(Channel channel) throws Exception {
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+        String message = "Read Last";
+        channel.basicPublish(EXCHANGE_NAME, Replica.ROUTING_KEY_READER , null, message.getBytes(StandardCharsets.UTF_8));
+        System.out.println(" [*] Sent '" + message + "'");
     }
 }
--- src/Replica.java
+++ src/Replica.java
@@ -1,17 +1,12 @@
 import com.rabbitmq.client.*;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class Replica {
-    private final static String EXCHANGE_NAME = "file_text_exchange";
-    private final static String EXCHANGE_READ = "read_exchange";
-    private final static String QUEUE_NAME_PREFIX = "file_text_queue_";
-
-    private static String replicaDirectory; // Répertoire du processus Replica
-    private static String fileName = "replica_file.txt"; // Nom du fichier local
+    protected final static String QUEUE_NAME_PREFIX = "replica_queue_";
+    protected static final String ROUTING_KEY_READER = "replica.";
+    private static String replicaDirectory;
+    private final static String fileName = "replica_file.txt";
 
     public static void main(String[] argv) throws Exception {
         if (argv.length != 1) {
@@ -22,86 +17,77 @@ public class Replica {
         int replicaNumber = Integer.parseInt(argv[0]);
         String queueName = QUEUE_NAME_PREFIX + replicaNumber;
 
-        replicaDirectory = "replica_" + replicaNumber; // Créer un répertoire unique pour chaque processus Replica
+        replicaDirectory = "replica_" + replicaNumber;
 
-        // Vérifier et créer le répertoire si nécessaire
         File directory = new File(replicaDirectory);
         if (!directory.exists()) {
             directory.mkdir();
         }
 
-//        processReadLast();
-
-        // Établir une connexion à RabbitMQ
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost"); // Adresse du broker RabbitMQ
+        factory.setHost("localhost");
         Connection connection = factory.newConnection();
         Channel channel = connection.createChannel();
-        // Déclarer la file d'attente correspondante
         channel.queueDeclare(queueName, false, false, false, null);
 
-        // Déclarer l'échange de type fanout
-        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
+        channel.exchangeDeclare(ClientWriter.EXCHANGE_NAME, BuiltinExchangeType.FANOUT);
+        channel.exchangeDeclare(ClientReader.EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
 
-        // Lier la file d'attente à l'échange
-        channel.queueBind(queueName, EXCHANGE_NAME, "");
-
-        channel.exchangeDeclare(EXCHANGE_READ, BuiltinExchangeType.FANOUT);
-        channel.queueBind(queueName, EXCHANGE_READ, "");
+        channel.queueBind(queueName, ClientWriter.EXCHANGE_NAME, "");
+        channel.queueBind(queueName, ClientReader.EXCHANGE_NAME, ROUTING_KEY_READER);
 
         System.out.println(" [*] Waiting for messages on replica " + replicaNumber + ". To exit press CTRL+C");
 
-        // Créer un consommateur pour recevoir les messages
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), "UTF-8");
-            System.out.println(" [x] Received '" + message + "'");
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [*] Received '" + message + "'");
             if (message.equals("Read Last")) {
                 try {
-                    processReadLast();
+                    String line = getLastLineOfFile(replicaDirectory + "/" + fileName);
+                    processReadLast(channel, line);
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
+            } else {
+                addLineToFile(replicaDirectory + "/" + fileName, message);
             }
-            addLineToFile(message);
         };
 
-        // Consommer les messages de la file d'attente
         channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
         });
-
     }
 
-    private static void addLineToFile(String line) {
+    private static void processReadLast(Channel channel, String message) throws Exception {
+        channel.exchangeDeclare(ClientReader.EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+        channel.basicPublish(ClientReader.EXCHANGE_NAME, ClientReader.ROUTING_KEY, null, message.getBytes(StandardCharsets.UTF_8));
+        System.out.println(" [*] Sent '" + message + "'");
+    }
+
+    private static void addLineToFile(String filePath, String line) {
         try {
-            // Créer le chemin complet du fichier dans le répertoire du Replica
-            String filePath = replicaDirectory + "/" + fileName;
             File file = new File(filePath);
-            file.createNewFile();
+            if (!file.exists() && !file.createNewFile()) System.out.println("file creation error");
 
-
-            // Écrire la ligne dans le fichier
             FileWriter fw = new FileWriter(file.getAbsoluteFile(), true);
             BufferedWriter bw = new BufferedWriter(fw);
             bw.write(line + "\n");
             bw.close();
-
-            System.out.println(" [x] Added line to file: " + line);
+//            System.out.println(" [x] Added line to file: " + line);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void processReadLast() throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        String message = "selem";
-
-        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
-            channel.exchangeDeclare("direct_read", BuiltinExchangeType.DIRECT);
-            channel.basicPublish("direct_read", "reader_queue", null, message.getBytes("UTF-8"));
-            System.out.println(" [x] Sent '" + "reader_queue" + "':'" + message + "'");
-
+    private static String getLastLineOfFile(String filePath) throws IOException {
+        String lastLine = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lastLine = line;
+            }
         }
-        System.out.println("Jeni read");
+        return lastLine;
     }
+
 }
+
